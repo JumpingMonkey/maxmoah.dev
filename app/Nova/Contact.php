7d79ecb4a46e6ef4003e11c0cc33a631e56908eb
morozovprav <morozovprav@gmail.com>
2021-07-01 11:28:36 +0300

    add contact page model, migration, respurce, controller.

---
+++ app/Nova/Contact.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Nova;
+
+use Digitalcloud\MultilingualNova\Multilingual;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Whitecube\NovaFlexibleContent\Flexible;
+
+class Contact extends Resource
+{
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Static Pages';
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Contact::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            Multilingual::make('Language'),
+            ID::make(__('ID'), 'id')->sortable(),
+            Flexible::make('Subject field', 'subject_field')
+            ->addLayout('Subject', 'subject', [
+                Text::make('Subject title', 'subj_title'),
+            ])->button('Add subject variant'),
+            Text::make('Button title', 'button_title'),
+            Text::make('Phone number', 'phone_number'),
+            Text::make('Email for press inquires', 'email_for_press_inquires'),
+            Text::make('Email general', 'email_general'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
---
+++ database/migrations/2021_07_01_061216_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->json('subject_field')->nullable();
+            $table->json('button_title')->nullable();
+            $table->json('phone_number');
+            $table->json('email_for_press_inquires');
+            $table->json('email_general');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
